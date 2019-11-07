d868169c77edaf3a67ad8580ed8dc6d1daec56e8
SCUPRISCILLA <941705864@qq.com>
2019-11-07 15:41:02 +0800

    robot can move to target pose

--- crp09_test/src/test_custom.cpp
+++ crp09_test/src/test_custom.cpp
@@ -1,6 +1,6 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include<moveit/planning_scene_interface/planning_scene_interface.h>
-#include<moveit/move_group_interface/move_group_interface.h>
+
 #include<moveit_msgs/DisplayRobotState.h>
 #include<moveit_msgs/DisplayTrajectory.h>
 #include<moveit_msgs/AttachedCollisionObject.h>
@@ -9,6 +9,7 @@
 #include<iostream>
 using namespace std;
 
+//每当修改，都要重新catkin_make
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "move_group_interface_tutorial");
@@ -18,39 +19,33 @@ int main(int argc, char **argv)
     sleep(2.0);
 
     moveit::planning_interface::MoveGroupInterface group("arm");
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+    //moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-    ros::Publisher display_publisher  = node_handle.advertise<moveit_msgs::DisplayTrajectory> ("/move_group/display_planned_path", 1, true);
-    moveit_msgs::DisplayTrajectory display_trajectory;
+   // ros::Publisher display_publisher  = node_handle.advertise<moveit_msgs::DisplayTrajectory> ("/move_group/display_planned_path", 1, true);
+  //  moveit_msgs::DisplayTrajectory display_trajectory;
     ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 
     geometry_msgs::Pose target_pose1;
 
   
-  target_pose1.orientation.x= -0.37;
-  target_pose1.orientation.y = 0.73;
-  target_pose1.orientation.z = 0.54;
-  target_pose1.orientation.w = 0.54;
+ target_pose1.orientation.x= 0.343915;
+  target_pose1.orientation.y = -0.000155354;
+  target_pose1.orientation.z = -0.000143756;
+  target_pose1.orientation.w = 0.939001;
 
-  target_pose1.position.x = -0.24;
-  target_pose1.position.y = 0.365518;
-  target_pose1.position.z = 1.16138;
+  target_pose1.position.x = -0.0656481;
+  target_pose1.position.y = 0.415693;
+  target_pose1.position.z = 1.04234;
 
     group.setPoseTarget(target_pose1);
 
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
     moveit::planning_interface::MoveItErrorCode success = group.plan(my_plan);
-    //ROS_INFO("Visualizing plan 1 (pose goal) %s", success.val ? "" : "Falled");
-    if(success.val)
-    {
+    ROS_INFO("Visualizing plan 1 (pose goal) %s", success ? "WHY SUCCESS" : "FUCKING FALLED");
+    if(success)
       group.execute(my_plan);
-      cout<<"WHY SUCCESS"<< endl;
-    }
-    if(!success.val)
-    {
-      cout<<"FUCKING FALLED"<<endl;
-    }
-          
+    else
+      cout << "falled"<<endl;
     sleep(5.0);
     ros::shutdown();
 }
