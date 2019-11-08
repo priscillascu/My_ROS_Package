720de004f2729aea9bd2f6e5c82cd0b2e14630bb
SCUPRISCILLA <941705864@qq.com>
2019-11-08 23:39:35 +0800

    using C++ to control target position

--- crp09_test/src/test_custom.cpp
+++ crp09_test/src/test_custom.cpp
@@ -7,45 +7,97 @@
 #include<moveit_msgs/CollisionObject.h>
 
 #include<iostream>
+#include <cmath>
+
+#define PI 3.14
 using namespace std;
+
+class RPY2qua  //定义一个RPY角转为四元数的类
+{
+   public:
+	double roll_deg;
+	double pitch_deg;
+	double yaw_deg;
+ 
+      // 成员函数声明
+    double getQua_x(void);
+    double getQua_y(void);
+    double getQua_z(void);
+    double getQua_w(void);
+};
+ 
+// 成员函数定义
+double RPY2qua::getQua_x(void)
+{
+	return sin(roll_deg*PI/180.0/2)*cos(yaw_deg*PI/180.0/2)*cos(pitch_deg*PI/180.0/2)-cos(roll_deg*PI/180.0/2)*sin(yaw_deg*PI/180.0/2)*sin(pitch_deg*PI/180.0/2);
+}
+
+double RPY2qua::getQua_y(void)
+{
+	return cos(roll_deg*PI/180.0/2)*sin(yaw_deg*PI/180.0/2)*cos(pitch_deg*PI/180.0/2)+sin(roll_deg*PI/180.0/2)*cos(yaw_deg*PI/180.0/2)*sin(pitch_deg*PI/180.0/2);
+}
+double RPY2qua::getQua_z(void)
+{
+	return cos(roll_deg*PI/180.0/2)*cos(yaw_deg*PI/180.0/2)*sin(pitch_deg*PI/180.0/2)-sin(roll_deg*PI/180.0/2)*sin(yaw_deg*PI/180.0/2)*cos(pitch_deg*PI/180.0/2);
+}
+double RPY2qua::getQua_w(void)
+{
+	return cos(roll_deg*PI/180.0/2)*cos(yaw_deg*PI/180.0/2)*cos(pitch_deg*PI/180.0/2)+sin(roll_deg*PI/180.0/2)*sin(yaw_deg*PI/180.0/2)*sin(pitch_deg*PI/180.0/2);
+}
 
 //每当修改，都要重新catkin_make
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "move_group_interface_tutorial");
+    RPY2qua my_RPY2qua;
+    ros::init(argc, argv, "my_crp09_moveit");
     ros::NodeHandle node_handle;
     ros::AsyncSpinner spinner(1);
     spinner.start();
     sleep(2.0);
 
     moveit::planning_interface::MoveGroupInterface group("arm");
-    //moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-   // ros::Publisher display_publisher  = node_handle.advertise<moveit_msgs::DisplayTrajectory> ("/move_group/display_planned_path", 1, true);
-  //  moveit_msgs::DisplayTrajectory display_trajectory;
     ROS_INFO("Reference frame: %s", group.getEndEffectorLink().c_str());
 
     geometry_msgs::Pose target_pose1;
 
-  
- target_pose1.orientation.x= 0.343915;
-  target_pose1.orientation.y = -0.000155354;
-  target_pose1.orientation.z = -0.000143756;
-  target_pose1.orientation.w = 0.939001;
+  //角度
+  //输入绕固定坐标轴x的角度，单位°
+   my_RPY2qua.roll_deg = -45;
+   //输入绕固定坐标轴y的角度，单位°
+   my_RPY2qua.yaw_deg = 0;
+   //输入绕固定坐标轴z的角度，单位° 
+   my_RPY2qua.pitch_deg = 0;
+   //使用我们编写的转换类，将RPY转为四元数
+   target_pose1.orientation.x= my_RPY2qua.getQua_x();
+    target_pose1.orientation.y = my_RPY2qua.getQua_y();
+    target_pose1.orientation.z = my_RPY2qua.getQua_z();
+    target_pose1.orientation.w = my_RPY2qua.getQua_w();
 
-  target_pose1.position.x = -0.0656481;
-  target_pose1.position.y = 0.415693;
-  target_pose1.position.z = 1.04234;
+//输入末端相对固定坐标系的位置
+    target_pose1.position.x = 0;
+    target_pose1.position.y = 0.345;
+    target_pose1.position.z = 0.876;
 
     group.setPoseTarget(target_pose1);
 
     moveit::planning_interface::MoveGroupInterface::Plan my_plan;
     moveit::planning_interface::MoveItErrorCode success = group.plan(my_plan);
-    ROS_INFO("Visualizing plan 1 (pose goal) %s", success ? "WHY SUCCESS" : "FUCKING FALLED");
+
     if(success)
+    {
+      cout << "Nice! You got it!"<<endl;
+      cout << "Executing your plan..." << endl;
       group.execute(my_plan);
+      cout << "In position! Go on!" << endl;
+    }
+      
     else
-      cout << "falled"<<endl;
+    {
+       cout << "Oh no, it's falled!!!"<<endl;
+       cout << "Please try again..." << endl;
+    }
+     
     sleep(5.0);
     ros::shutdown();
 }
